a95728a437bcf925cd407b42b22d9f4981408d8c
Полешко Роман Олегович <romanpolesko@gmail.com>
2017-11-27 11:26:17 +0300

    first+, second one does not work

---
+++ 13work/effects/script.js
@@ -0,0 +1,39 @@
+﻿var canvas = document.getElementsByClassName("canvas")[0];
+var context = canvas.getContext("2d");
+var butBW = document.getElementsByClassName("black-white")[0];
+var butNegative = document.getElementsByClassName("negative")[0];
+var butCrema = document.getElementsByClassName("crema")[0];
+var img = document.getElementsByClassName("bmw")[0];
+
+context.drawImage(img, 0, 0);
+
+butBW.addEventListener("click", function(){
+	this.classList.toggle("press");
+	for (var pixelx = 1; pixelx < 600; pixelx++)
+	for (var pixely = 1; pixely < 600; pixely++)
+	{
+		var sum = 0;
+		sum += context.getImageData(pixelx, pixely,1,1).data[0];
+		sum += context.getImageData(pixelx, pixely,1,1).data[1];
+		sum += context.getImageData(pixelx, pixely,1,1).data[2];
+		sum = Math.round(sum / 3);
+		context.putImageData(sum, pixelx, pixely);
+		
+	}
+	console.log("gotovo");
+	butNegative.classList.remove("press");
+	butCrema.classList.remove("press");
+})
+
+butNegative.addEventListener("click", function(){
+	this.classList.toggle("press");
+	butBW.classList.remove("press");
+	butCrema.classList.remove("press");
+
+})
+
+butCrema.addEventListener("click", function(){
+	this.classList.toggle("press");
+	butNegative.classList.remove("press");
+	butBW.classList.remove("press");
+})
---
+++ 13work/karandash/script.js
@@ -0,0 +1,14 @@
+﻿var canvas = document.getElementsByClassName("canvas")[0];
+var context = canvas.getContext("2d");
+var x = window.pageX, y = window.pageY, xnew, ynew;
+
+canvas.addEventListener("mousemove", function(e){
+	context.strokeStyle = "purple";
+	context.beginPath();
+	xnew = x; ynew = y;
+	context.moveTo(xnew, ynew);
+	context.lineTo(e.pageX, e.pageY);
+	context.stroke();
+	x = e.pageX; y = e.pageY;
+});
+
